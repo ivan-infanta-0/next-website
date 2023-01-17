825fc62549bc6f681616d4ac758e9fe25284617c
Corvus Necromyst <ivanhayz.gonzales@gsfe.tupcavite.edu.ph>
2023-01-17 17:40:43 +0800

    replace props.action with onSubmit

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -5,15 +5,19 @@ var bcrypt = require('bcryptjs');
 var salt = bcrypt.genSaltSync(10);
 
 export default function handler(req, res) {
-  const body = req.body;
+  console.log('req.body:');
+  console.log(req.body);
   
+  const email = req.body.email;
   const username = req.body.username;
   const password = req.body.password;
   
-  // do a database insert here
-  
-  
-  res.status(200).send({ok: true});
+  try {
+    // do a database insert here
+    res.send({ok: true});
+  } catch (error) {
+    res.send({ok: false});
+  }
 }
 
 // {user: body.username, pass: bcrypt.hashSync(body.password, salt)}
--- pages/signup.js
+++ pages/signup.js
@@ -24,6 +24,7 @@ export const getServerSideProps = withSessionSsr(async (context) => {
 export default function Signup() {
   const router = useRouter();
   
+  const emailInput = useRef();
   const usernameInput = useRef();
   const passwordInput = useRef();
   const repeatpwInput = useRef();
@@ -32,29 +33,33 @@ export default function Signup() {
   const [displayIncorrectInputMessage, toggleIncorrectInputMessage] = useState(false);
   
   const handleSubmit = async (e) => {
-    e.preventdefault();
+    e.preventDefault();
     
+    const email = emailInput.current.value;
     const username = usernameInput.current.value;
     const password = passwordInput.current.value;
     const repeatpw = repeatpwInput.current.value;
     
     if (password !== repeatpw) {
-      return // X!!!! ABORT! (wip)
+      return toggleIncorrectInputMessage(true)
     }
     
     const res = await fetch('/api/signup', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({username, password})
+      body: JSON.stringify({email, username, password})
     });
     
     if (res.ok) {
       // login immediately after creating an account
+      console.log("signup succ"); // /!\ outputs to browser console /!\
     }
   }
   
   return (
-    <form action="/api/signup" method="post">
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="femail">email</label>
+      <input type="email" id="femail" name="email" ref={emailInput} required />
       <label htmlFor="fusername">username</label>
       <input type="text" id="fusername" name="username" ref={usernameInput} required />
       <label htmlFor="fpassword">password</label>
@@ -62,6 +67,7 @@ export default function Signup() {
       <label htmlFor="frepeatpw">password</label>
       <input type="password" id="frepeatpw" name="repeatpw" ref={repeatpwInput} required />
       <button type="submit">submit</button>
+      <p hidden={!displayIncorrectInputMessage}>invalid credentials -- username already exists; passwords do not match, etc.</p>
     </form>
   )
 }
